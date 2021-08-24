7c5ed1b9b8a7c86cf939752703698cc38a8d502f
tt-tt-c <tsuruga10415@gmail.com>
2021-08-24 15:37:13 +0900

    complete

--- src/index.tsx
+++ src/index.tsx
@@ -1,67 +1,133 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-class Square extends React.Component {
-   render() {
-       return (
-           <button className="square">
-               {/* TODO */}
-           </button>
-       );
-   }
+interface SquarePropsInterface {
+    value: string;
+    onClick: () => void;
 }
 
-class Board extends React.Component {
-   renderSquare(i:number) {
-       return <Square />;
-   }
+interface SquareStateInterface {
+    value: string;
+}
 
-   render() {
-       const status = 'Next player: X';
+function Square(props: SquarePropsInterface) {
+    return (
+        <button
+            className="square"
+            onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    )
+}
 
-       return (
-           <div>
-               <div className="status">{status}</div>
-               <div className="board-row">
-                   {this.renderSquare(0)}
-                   {this.renderSquare(1)}
-                   {this.renderSquare(2)}
-               </div>
-               <div className="board-row">
-                   {this.renderSquare(3)}
-                   {this.renderSquare(4)}
-                   {this.renderSquare(5)}
-               </div>
-               <div className="board-row">
-                   {this.renderSquare(6)}
-                   {this.renderSquare(7)}
-                   {this.renderSquare(8)}
-               </div>
-           </div>
-       );
-   }
+interface BoardPropsInterface {
+    squares: string[];
+    xIsNext: boolean
+}
+
+interface BoardStateInterface {
+    squares: string[];
+    xIsNext: boolean
+    winner: string
+}
+
+class Board extends React.Component<BoardPropsInterface, BoardStateInterface> {
+    constructor(props: BoardPropsInterface) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(""),
+            xIsNext: true,
+            winner: "",
+        };
+    }
+
+    handleClick(i: number) {
+        const winner = calculateWinner(this.state.squares);
+        if(winner || this.state.squares[i]) return
+
+        const squares: string[] = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? "X" : "O";
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+            winner: winner
+        });
+    }
+
+    renderSquare(i: number) {
+        return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+    }
+
+    render() {
+        let status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+
+        const winner = calculateWinner(this.state.squares);
+
+       if (winner) {
+           status = 'Winner: ' + winner;
+       }
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
 }
 
 class Game extends React.Component {
-   render() {
-       return (
-           <div className="game">
-               <div className="game-board">
-                   <Board />
-               </div>
-               <div className="game-info">
-                   <div>{/* status */}</div>
-                   <ol>{/* TODO */}</ol>
-               </div>
-           </div>
-       );
-   }
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board squares={Array(9).fill("")} xIsNext={true} />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
 }
 
 // ========================================
 
-ReactDOM.render(
-   <Game />,
-   document.getElementById('root')
-);
+ReactDOM.render(<Game />, document.getElementById("root"));
+
+function calculateWinner(squares: Array<string>): string {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return "";
+ }
